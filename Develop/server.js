f2656d8e9b614f5528292ac62c04d1d2bb8c8b18
marquisegordon <shailomg@gmail.com>
2022-11-30 23:39:21 -0700

    created server.js

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+//create express app
+const app = express();
+
+//create a PORT variable
+const PORT = process.env.PORT || 3001;
+
+//set up express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const apiRoutes = require('./routes/api.js');
+app.use(apiRoutes);
+const htmlRoutes = require('./routes/html');
+app.use(htmlRoutes);
+
+//create server listener
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
