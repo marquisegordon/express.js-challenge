da5bdaed2859da548ac063563ffbc41649e143bf
marquisegordon <shailomg@gmail.com>
2022-11-30 23:25:10 -0700

    add html, api routes, and helper files

---
+++ Develop/routes/api.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend } = require('../helpers/helpers.js');
+
+router.get('/api/notes', (req, res) => {
+    readFromFile('../db/db.json').then((data) =>
+        res.json(JSON.parse(data))
+    );
+});
+
+router.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+          title,
+          text,
+          id: uuidv4(),
+        };
+    
+        readAndAppend(newNote, './db/tips.json');
+        res.json(`Note added successfully`);
+      } else {
+        res.error('Error in adding tip');
+      }
+});
+
+module.exports = router;
